b3fe33ce2be9271202f67534824520d9752e6c46
Ronu Ghoshal <email": null>
2016-04-02 15:55:40 -0700

    Work on check_column method

---
+++ application.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $( 'button' ).on('')
+
+
+})
+
+var find_open_cell = function(column) {
+  string = column.split(' ')[0].toString();
+  var cells_in_column = []
+  $( '.' + column + ' li div' )
+};
