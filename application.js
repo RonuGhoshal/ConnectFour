f97acfbf9373436fe2971f29ddbb6a402daf01ae
Ronu Ghoshal <email": null>
2016-04-02 16:57:04 -0700

    Work on methods

--- application.js
+++ application.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   $( 'button' ).on('')
-
+  var open_cell = find_open_cell(column1);
+  change_cell_color(open_cell);
 
 })
 
 var find_open_cell = function(column) {
   string = column.split(' ')[0].toString();
-  var cells_in_column = $( '.' + column + ' li div' );
+  var cells_in_column = $( '#' + column + ' li div' );
   for (i = 5; i>=0; i--) {
     if ($(cells_in_column[i]).attr("class").includes("nopiece")) {
       return $(cells_in_column[i]).id;
@@ -23,5 +24,13 @@ var change_cell_color = function(cell) {
 
   else if (current player == "black") {
     cell.addClass("blackpiece");
-  }
+  };
+
+var checkForWin = function(cell) {
+
+}
+
+var checkVertical = function(cell){
+
+}
 };
