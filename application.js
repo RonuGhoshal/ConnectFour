027ed65921a1fa556e2f736fb22b7f6e4f4b18de
Ronu Ghoshal <email": null>
2016-04-02 16:12:13 -0700

    Finish find_open_cell function

--- application.js
+++ application.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
 
 var find_open_cell = function(column) {
   string = column.split(' ')[0].toString();
-  var cells_in_column = []
-  $( '.' + column + ' li div' )
+  var cells_in_column = $( '.' + column + ' li div' );
+  for (i = 5; i>=0; i--) {
+    if ($(cells_in_column[i]).attr("class").includes("nopiece")) {
+      return $(cells_in_column[i]).id;
+    }
+  }
+  return "all cells full";
 };
+
