6d3499dcbf6bc7f22f47c4285139408e20a357c1
Ian Thorp & Sharon <pair+IanThorp.sharonjean@devbootcamp.com>
2016-04-02 18:41:54 -0700

    Refactor column selector

--- connect4style.js
+++ connect4style.js
@@ -1,72 +1,18 @@
 $(document).ready(function(){
-// test
-columnSelector1();
-columnSelector2();
-columnSelector3();
-columnSelector4();
-columnSelector5();
-columnSelector6();
-columnSelector7();
+
+columnSelector();
+
 
 });
 
 
-var columnSelector1 = function(){
-  $("#column1").on("click", function(event){
+var columnSelector = function(){
+  $(".standardcolumn").on("click", function(event){
     event.preventDefault();
-    return this.id;
+    var column_id = this.id;
+    return column_id;
   });
 };
-
-var columnSelector2 = function(){
-  $("#column2").on("click", function(event){
-    event.preventDefault();
-    return this.id;
-  });
-};
-
-var columnSelector3 = function(){
-  $("#column3").on("click", function(event){
-    event.preventDefault();
-    return this.id;
-  });
-};
-
-var columnSelector4 = function(){
-  $("#column4").on("click", function(event){
-    event.preventDefault();
-    return this.id;
-  });
-};
-
-var columnSelector5 = function(){
-  $("#column5").on("click", function(event){
-    event.preventDefault();
-    return this.id;
-  });
-};
-
-
-
-var columnSelector6 = function(){
-  $("#column6").on("click", function(event){
-    event.preventDefault();
-    return this.id;
-  });
-};
-
-var columnSelector7 = function(){
-  $("#column7").on("click", function(event){
-    event.preventDefault();
-    return this.id;
-  });
-
-
-};
-
-var dropRed = function(){
-`
- };
 
 
 
